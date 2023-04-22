a5d20214682bdd569698f2bc2d2b1e1e3f517139
Willian Pereira <williansouza9687a@gmail.com>
2023-04-21 22:18:43 -0300

    completed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/globals'
+import Logo from '../assets/logo.png'
+import { Container, Header } from '../styles/pages/app';
+import Image from 'next/image';
+import Link from 'next/link';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Container>
+
+
+      <Header>
+        <Link href="/">
+          <Image src={Logo} alt='' />
+        </Link>
+
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
